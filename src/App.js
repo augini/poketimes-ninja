899122688166bb081f6924238f208387abb13d1e
Farrukh Atabekov <augini18@gmail.com>
2020-07-10 02:30:52 +0900

    --update: deploy website to github

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Home from './components/Home'
-import Contact from './components/Contact'
-import About from './components/About'
-import Post from './components/Post'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Post from "./components/Post";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/about' component={About} />
-          <Route path='/:post_id' component={Post} />
+          <Route exact path="" component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+          <Route path="/:post_id" component={Post} />
         </Switch>
       </div>
     </BrowserRouter>
