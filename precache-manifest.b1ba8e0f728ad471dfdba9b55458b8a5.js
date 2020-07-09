be3d5c655d63c74aa80269fdedaea3ab612d87ed
Farrukh Atabekov <augini18@gmail.com>
2020-07-10 02:03:04 +0900

    Updates

---
+++ precache-manifest.b1ba8e0f728ad471dfdba9b55458b8a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ab4957855c84d4b43029a00f5e910d",
+    "url": "/poketimes-ninja/index.html"
+  },
+  {
+    "revision": "442bc05935c78a2abdc0",
+    "url": "/poketimes-ninja/static/css/main.39a8ae0c.chunk.css"
+  },
+  {
+    "revision": "76c14c3a4fb1d8c4bab9",
+    "url": "/poketimes-ninja/static/js/2.40b98e3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poketimes-ninja/static/js/2.40b98e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442bc05935c78a2abdc0",
+    "url": "/poketimes-ninja/static/js/main.6124da86.chunk.js"
+  },
+  {
+    "revision": "9d70e1e7f658563aea82",
+    "url": "/poketimes-ninja/static/js/runtime-main.b11c1271.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimes-ninja/static/media/pokeball.f10bf078.png"
+  }
+]);
